14e85205b39b0ef732ba4cec29a4b153e3dd934f
Francisco Picoto Fixo <francisco.picoto@hotmail.com>
2024-05-14 21:00:36 +0100

    FP - Character base code

---
+++ background.js
@@ -0,0 +1,43 @@
+class Background extends Entity 
+{
+	constructor(spriteSheet, x, y) {
+		super();
+		this.states = {
+			UNIQUE: 'UNIQUE'
+		};
+	
+		this.spriteSheet = spriteSheet;
+		this.x = x;
+		this.y = y;
+		this.currentState = this.states.UNIQUE;
+		this.currentFrame = 0;
+		this.vx = 0;
+		this.vy = 0;
+		this.setup();
+	}
+	  
+	update() {
+		this.x += this.vx;
+        this.y += this.vy;
+		if (this.x == Math.round(this.width / 3 * 2))
+			this.x = 0;
+        if (this.y == Math.round(this.height / 3 * 2))
+			this.y = 0;
+	}
+
+	getSprite() {
+		return this.frames[this.currentFrame];
+	}
+
+	setup() {
+		this.eStates.UNIQUE = this.spriteSheet.getStats('UNIQUE');
+		this.frames = this.eStates[this.currentState];
+		this.width = this.frames[0].width;
+		this.height = this.frames[0].height; 
+	}
+
+	stop() {
+		this.vx = 0;
+        this.vy = 0;
+	}
+}
--- character.js
+++ character.js
@@ -1,12 +1,95 @@
 class Character extends Entity {
-    constructor () {
+    constructor (spriteSheet, x, y, canvasWidth, canvasHeight) {
+        super();
+        this.states = {
+            MOVE: 'MOVE',
+            SHOOT: 'SHOOT',
+            STOPPED: 'STOPPED',
+            HIT: 'HIT'
+        };
+      
+        this.direction = {
+            LEFT: 'LEFT',
+            RIGHT: 'RIGHT',
+            UP: 'UP',
+            DOWN: 'DOWN'
+        }
+
+        this.spriteSheet = spriteSheet;
+        this.x = x;
+        this.y = y;
+        this.currentState = this.states.STOPPED;
+        this.currentFrame = 0;
+        this.vx = 20;
+        this.vy = 20;
+        this.canvasWidth = canvasWidth;
+        this.canvasHeight = canvasHeight;
+
         this.valueHP = 100;
         this.valueCoins = 0;
         this.valueExp = 0;
         this.valueAtk = 5;
         this.valueDef = 5;
         this.level = 1;
+
+        this.setup();
     }
+
+    update() {
+        this.currentFrame = (++this.currentFrame) % this.frames.length;
+      this.width = this.frames[this.currentFrame].width;
+      this.height = this.frames[this.currentFrame].height;
+    }
+    
+    getSprite() {
+        return this.frames[this.currentFrame];
+    }
+
+    setup() {
+        this.eStates.MOVE = this.spriteSheet.getStats('ANDAR');
+      this.eStates.SHOOT = this.spriteSheet.getStats('DISPARAR');
+      this.eStates.STOPPED = this.spriteSheet.getStats('PARADO');
+      this.eStates.HIT = this.spriteSheet.getStats('ATINGIDO');
+  
+      this.frames = this.eStates[this.currentState];
+      this.width = this.frames[0].width;
+      this.height = this.frames[0].height;
+    }
+
+    move(direction) {
+        this.toggleState(this.states.MOVE);
+        
+        switch (direction) {
+          case this.direction.LEFT:
+            this.x -= this.x - this.vx >= 0 ? this.vx : 0;
+            break;
+          case this.direction.RIGHT:
+            this.x += this.x + this.vx <= this.canvasWidth - this.width ? this.vx : 0;
+            break;
+          case this.direction.UP:
+            this.y -= this.y - this.vy >= 0 ? this.vy : 0;
+            break;
+          case this.direction.DOWN:
+            this.y += this.y + this.vy <= this.canvasHeight - this.height ? this.vy : 0;
+            break;
+        }
+    }
+      
+    stop() {
+        this.toggleState(this.states.STOPPED);
+    }
+
+    shoot() {
+        this.toggleState(this.states.SHOOT);
+    }
+
+    toggleState(newState) {
+        if (this.currentState === newState)
+          return;
+        this.currentState = newState;
+        this.frames = this.eStates[this.currentState];
+        this.currentFrame = 0;
+    } 
 
     // Subir o nivel
     levelUp() {
